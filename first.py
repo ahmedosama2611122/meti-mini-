1e206a1e4968cdb7a22263953dd7aaff574ca5db
ahmedosama2611122 <ahmed.osama2@meti.dev>
2024-12-30 18:48:31 +0200

    Add files via upload

---
+++ first.py
@@ -0,0 +1,3 @@
+print("Hello! My name is [ahmed osama].")
+print("I am [24] years old.")
+print("In this project, I want to learn how to write basic Python programs, use variables, and work with functions.")
